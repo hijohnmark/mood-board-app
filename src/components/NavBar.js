d5f5cdcb61d14e41ae507f7599b0cfe00baddd44
John Mark Harrell <hijohnmark@gmail.com>
2024-08-04 10:39:01 -0700

    build NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <nav>
+            <NavLink 
+            to="/"
+            className="nav-link"
+            >
+                Home
+            </NavLink>
+            <NavLink
+            to="/newpost"
+            className="nav-link"
+            >
+                New Post
+            </NavLink>
+            <NavLink 
+            to="/about"
+            className="nav-link"
+            >
+                About
+            </NavLink>
+        </nav>
+    )
+}
+
+export default NavBar
