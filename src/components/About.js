6750c7cf5453c6c29e5224879c2486e84a0c81c2
John Mark Harrell <hijohnmark@gmail.com>
2024-08-04 10:41:59 -0700

    Add NavBar to About, PostForm components

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
+import NavBar from './NavBar'
+
 const About = () => {
     return(
         <div>
+            <NavBar />
             <h1>About</h1>
         </div>
     )
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,9 @@
+import NavBar from "./NavBar"
+
 const PostForm = () => {
     return (
         <div>
+            <NavBar />
             <h1>Form</h1>
         </div>
     )
