cdfbd959d116a994bbee27fbe75b0abb18ed0ac0
John Mark Harrell <hijohnmark@gmail.com>
2024-09-05 14:23:45 +0700

    add form clearing functionality

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -25,6 +25,10 @@ const PostForm = () => {
     })
     .then(r => r.json())
     .then(newPost => onAddPost(newPost))
+
+        setTitle("")
+        setImage("")
+        setDescription("")
     }
 
     return (
